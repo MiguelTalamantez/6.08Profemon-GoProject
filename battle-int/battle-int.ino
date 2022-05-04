8e282c0182787dbe22532f4b0223104176ea7162
Audrey Lee <audrey16@mit.edu>
2022-05-04 04:06:51 -0400

    Start implementing battle

--- battle-int/battle-int.ino
+++ battle-int/battle-int.ino
@@ -9,25 +9,29 @@
 #include <ArduinoJson.h>
 #include <WiFiClientSecure.h>
 #include <WiFiClient.h>
+#include <stdio.h>
+#include <cstring>
 
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 MPU6050 imu; //imu object called, appropriately, imu
-const int BUTTON1 = 45;
-const int BUTTON2 = 39;
-const int BUTTON3 = 36;
+WiFiClientSecure client; //global WiFiClient Secure object
+WiFiClient client2; //global WiFiClient Secure object
+
+const int BUTTON1 = 45; //Button1 at pin 45
+const int BUTTON2 = 39; //Button2 at pin 39
+const int BUTTON3 = 38; //Button3 at pin 38
+const int BUTTON4 = 34; //Button4 at pin 34
 
 // HTTP Request Variables
 const int RESPONSE_TIMEOUT = 6000; //ms to wait for response from host
 const int GETTING_PERIOD = 2000; //periodicity of getting a number fact.
 const int BUTTON_TIMEOUT = 1000; //button timeout in milliseconds
-const uint16_t IN_BUFFER_SIZE = 4000; //size of buffer to hold HTTP request
-const uint16_t OUT_BUFFER_SIZE = 1000; //size of buffer to hold HTTP response
+const uint16_t IN_BUFFER_SIZE = 5000; //size of buffer to hold HTTP request
+const uint16_t OUT_BUFFER_SIZE = 5000; //size of buffer to hold HTTP response
 const uint16_t JSON_BODY_SIZE = 3000;
 char request_buffer[IN_BUFFER_SIZE]; //char array buffer to hold HTTP request
 char response_buffer[OUT_BUFFER_SIZE]; //char array buffer to hold HTTP response
 char json_body[JSON_BODY_SIZE];
-WiFiClientSecure client; //global WiFiClient Secure object
-WiFiClient client2; //global WiFiClient Secure object
 StaticJsonDocument<500> doc;
 uint8_t channel = 1; //network channel on 2.4 GHz
 byte bssid[] = {0x04, 0x95, 0xE6, 0xAE, 0xDB, 0x41}; //6 byte MAC address of AP you're targeting.
@@ -37,6 +41,10 @@ const char LOC_PREFIX[] = "{\"wifiAccessPoints\": ["; //beginning of json body
 const char LOC_SUFFIX[] = "]}"; //suffix to POST request
 const char API_KEY[] = "AIzaSyAQ9SzqkHhV-Gjv-71LohsypXUH447GWX8"; //don't change this and don't share this
 const int MAX_APS = 5;
+
+const char AUDIO_PREFIX[] = "{\"config\":{\"encoding\":\"MULAW\",\"sampleRateHertz\":8000,\"languageCode\": \"en-US\",\"speechContexts\":[{\"phrases\":[\"riemann\", \"jacobian\"]}]}, \"audio\": {\"content\":\"";
+const char AUDIO_SUFFIX[] = "\"}}"; //suffix to POST request_buffer
+const int AUDIO_IN = 1; //pin where microphone is connected
 
 char network[] = "MIT";
 char password[] = "";
@@ -69,7 +77,7 @@ uint32_t swing_times[30];
 bool swing_in_progress = false;
 
 // Game/Display Variables
-const char user[] = "Pikachu";
+const char user[50] = "Pikachu";
 uint8_t change_display = 0;
 uint8_t new_profemon = 0;
 char display_name[50] = "";
@@ -83,6 +91,70 @@ char* begin_profedex = 0;
 double latitude = 0.0;
 double longitude = 0.0;
 uint32_t loc_timer = 0;
+
+
+/*********************************************
+Battle Stuff
+*********************************************/
+// audio variables
+const int DELAY = 1000;
+const int SAMPLE_FREQ = 8000;                          // Hz, telephone sample rate
+const int SAMPLE_DURATION = 5;                        // duration of fixed sampling (seconds)
+const int NUM_SAMPLES = SAMPLE_FREQ * SAMPLE_DURATION;  // number of of samples
+const int ENC_LEN = (NUM_SAMPLES + 2 - ((NUM_SAMPLES + 2) % 3)) / 3 * 4;  // Encoded length of clip
+bool conn;
+uint32_t time_since_sample;      // used for microsecond timing
+char speech_data[ENC_LEN + 200] = {0}; //global used for collecting speech data
+char heard[200];
+
+// overall battle variables
+uint8_t b1;
+uint8_t b2;
+uint8_t b3;
+uint8_t old_b1_input;
+uint8_t old_b2_input;
+uint8_t old_b3_input;
+
+uint8_t num_turns;
+
+uint8_t gesture;
+uint8_t battle_state;
+
+char user[50];
+char profemon_id[50];
+uint8_t game_id;
+char standard_move_name[50];
+char special_move_name[50];
+
+uint8_t old_player_hp;
+uint8_t player_hp;
+uint8_t other_hp;
+uint8_t max_attack;
+char last_move[50];
+uint8_t damage_done;
+
+uint32_t time_since_turn_check; 
+const uint32_t turn_check_period = 500;
+uint32_t time_since_turn_started;
+const uint32_t max_turn_length = 300000;
+
+const uint8_t start = 0;
+const uint8_t turn = 1;
+const uint8_t attack = 2;
+const uint8_t forfeit = 3;
+
+// gesture recognition variables
+int gesture_duration = 5000;
+
+int battlemode;
+const int x_shake = 1;
+const int y_shake = 2;
+const int z_shake = 3;
+
+int start_time;
+
+int gesture_total;
+
 
 void setup() {
   Serial.begin(115200); //for debugging if needed.
@@ -129,6 +201,7 @@ void setup() {
   pinMode(BUTTON1, INPUT_PULLUP);
   pinMode(BUTTON2, INPUT_PULLUP);
   pinMode(BUTTON3, INPUT_PULLUP);
+  pinMode(BUTTON4, INPUT_PULLUP);
   loc_timer = millis();
 }
 
@@ -496,4 +569,326 @@ void update_location_and_profs() {
         }
         loc_timer = millis();
     }
+}
+
+
+void make_server_request(int type) {
+  request_buffer[0] = '\0'; //set 0th byte to null
+  offset = 0; //reset offset variable for sprintf-ing
+  
+  if(type == start) {
+    // send a POST request_buffer labeled "start" with the player_id and prof_id
+    request_buffer[0] = '\0';
+    offset = 0;
+    offset += sprintf(request_buffer + offset, "POST http://608dev-2.net/sandbox/sc/team3/battle_brain.py?label=start&player_id=%s&prof_id=%s  HTTP/1.1\r\n",user,profemon_id);
+    offset += sprintf(request_buffer + offset, "Host: 608dev-2.net\r\n\r\n");
+    do_http_request("608dev-2.net", request_buffer, response_buffer, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, false);
+
+    // info to store: game_id, gesture, our own hp (as both player_hp and old_player_hp)
+    char breaks[2];
+    breaks[0] = ',';
+    breaks[1] = '\0';
+    char* ptr = strtok(response_buffer, breaks);
+
+    //Serial.printf("1: %s",ptr);
+    game_id = atoi(ptr);
+    ptr = strtok (NULL, breaks);
+
+    //Serial.printf("2: %s",ptr);
+    gesture = atoi(ptr);
+    ptr = strtok (NULL, breaks);
+
+    //Serial.printf("3: %s",ptr);
+    player_hp = atoi(ptr);
+    old_player_hp = player_hp;
+
+    Serial.printf("GAME HAS BEGUN! id is %u\n",game_id);
+
+    // send a GET request_buffer to profedex.py asking for attack
+    request_buffer[0] = '\0';
+    offset = 0;
+    offset += sprintf(request_buffer + offset, "GET http://608dev-2.net/sandbox/sc/team3/profedex.py?professor=%s&item=attack  HTTP/1.1\r\n",profemon_id);
+    offset += sprintf(request_buffer + offset, "Host: 608dev-2.net\r\n\r\n");
+    do_http_request("608dev-2.net", request_buffer, response_buffer, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, false);
+
+    // info to store: our own attack value
+    max_attack = atoi(response_buffer);
+
+    Serial.printf("max attack score is: %u\n",max_attack);
+
+    // send ANOTHER request_buffer to figure out what your prof-emon's two moves are
+    request_buffer[0] = '\0';
+    offset = 0;
+    offset += sprintf(request_buffer + offset, "GET http://608dev-2.net/sandbox/sc/team3/profedex.py?professor=%s&item=moves  HTTP/1.1\r\n",profemon_id);
+    offset += sprintf(request_buffer + offset, "Host: 608dev-2.net\r\n\r\n");
+    do_http_request("608dev-2.net", request_buffer, response_buffer, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, false);
+
+    char* ending1 = strrchr(response_buffer, '\n');
+    *(ending1 + 0) = NULL;
+
+    //Serial.printf("%s\n",response);
+
+    // info to store: the two moves
+    ptr = strtok(response_buffer, breaks);
+    
+    memset(standard_move_name,0,50);
+    strcpy(standard_move_name,ptr);
+    ptr = strtok (NULL, breaks);
+
+    memset(special_move_name,0,50);
+    strcpy(special_move_name,ptr);
+
+    Serial.printf("The real moves are %s and %s\n", standard_move_name,special_move_name);
+  }
+  else if(type == turn) {
+    // send a GET request_buffer labeled "turn" with the game_id and player_id
+    offset += sprintf(request_buffer + offset, "GET http://608dev-2.net/sandbox/sc/team3/battle_brain.py?label=turn&player_id=%s&game_id=%u  HTTP/1.1\r\n",user,game_id);
+    offset += sprintf(request_buffer + offset, "Host: 608dev-2.net\r\n\r\n");
+    do_http_request("608dev-2.net", request_buffer, response_buffer, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, false);
+
+    // if the response is just "-1", the game hasn't started yet, and we should just keep waiting for the other player to enter the game
+    if(strncmp(response_buffer,"-1",2)==0) {
+      return;
+    }
+
+    else if(battle_state==1) {
+      Serial.printf("at the time, the turn is %s\n",response_buffer);
+    }
+
+    // info to store: the id of whose turn it is, our own hp, the other player's hp, and the last move
+    char breaks[2];
+    breaks[0] = ',';
+    breaks[1] = '\0';
+    char* ptr = strtok(response_buffer, breaks);
+    
+    if(strcmp(ptr,user)==0) {
+      Serial.printf("state is now 1\n");
+      battle_state = 1;
+    }
+
+    ptr = strtok (NULL, breaks);
+    player_hp = atoi(ptr);
+
+    ptr = strtok (NULL, breaks);
+    other_hp = atoi(ptr);
+
+    ptr = strtok (NULL, breaks);
+    if(strncmp(response_buffer,"n/a",3)==0) {
+      memset(last_move,0,50);
+    }
+    else {
+      memset(last_move,0,50);
+      strcpy(last_move,ptr);
+    }
+
+    // Serial.printf("this will never be reached :/");
+  }
+  else if(type == attack) {
+    // send a POST request_buffer labeled "attack" with the game_id, player_id, move, and damage
+    request_buffer[0] = '\0';
+    offset = 0;
+    offset += sprintf(request_buffer + offset, "POST http://608dev-2.net/sandbox/sc/team3/battle_brain.py?label=attack&player_id=%s&game_id=%u&move=%s&damage=%u  HTTP/1.1\r\n",user,game_id,last_move,damage_done);
+    offset += sprintf(request_buffer + offset, "Host: 608dev-2.net\r\n\r\n");
+    Serial.printf("sending an attack request_buffer with url %s\n",request_buffer);
+    do_http_request("608dev-2.net", request_buffer, response_buffer, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, false);
+    Serial.printf("request_buffer has been sent\n");
+  }
+  else if(type == forfeit) {
+    // send a POST request_buffer labeled "forfeit" with the game_id and player_id
+    request_buffer[0] = '\0';
+    offset = 0;
+    offset += sprintf(request_buffer + offset, "POST http://608dev-2.net/sandbox/sc/team3/battle_brain.py?label=forfeit&player_id=%s&game_id=%u  HTTP/1.1\r\n",user,game_id);
+    offset += sprintf(request_buffer + offset, "Host: 608dev-2.net\r\n\r\n");
+    do_http_request("608dev-2.net", request_buffer, response_buffer, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, false);
+  }
+}
+
+int battle_direction(int what_to_do){
+  battlemode = what_to_do;
+  gesture_total = 0;
+  switch(battlemode){
+    case x_shake:
+    tft.println("SHAKE LEFT AND RIGHT");
+      while((millis() - start_time) < gesture_duration){
+        imu.readAccelData(imu.accelCount);
+        x = abs(ZOOM * imu.accelCount[0] * imu.aRes);
+
+        x_avg = (x + x_old + x_older) / 3.0;
+        x_older = x_old;
+        x_old = x;
+        
+        // char output[100];
+        // sprintf(output,"X:%4.2f", x_avg); 
+        // Serial.println(output);
+
+        if(x_avg > 6){
+          gesture_total += 1;
+        }
+      }
+      break;
+    case y_shake:
+      tft.println("SHAKE UP AND DOWN");
+      while((millis() - start_time) < gesture_duration){
+        imu.readAccelData(imu.accelCount);
+        y = abs(ZOOM * imu.accelCount[1] * imu.aRes);
+
+        y_avg = (y + y_old + y_older) / 3.0;
+        y_older = y_old;
+        y_old = y;
+
+        // char output[100];
+        // sprintf(output,"Y:%4.2f", y_avg); 
+        // Serial.println(output);
+
+        if(y_avg > 12){
+          gesture_total += 1;
+        }
+      }
+      break;
+    case z_shake:
+      tft.println("SHAKE FORWARD AND BACK");
+      while((millis() - start_time) < gesture_duration){
+        imu.readAccelData(imu.accelCount);
+        z = abs(ZOOM * imu.accelCount[2] * imu.aRes);
+
+        z_avg = (z + z_old + z_older) / 3.0;
+        z_older = z_old;
+        z_old = z;
+
+        //char output[100];
+        //sprintf(output,"Z:%4.2f", z_avg); 
+        //Serial.println(output);
+
+        if(z_avg > 6){
+          gesture_total += 1;
+        }
+      }
+      break;
+  }
+
+  return (gesture_total*max_attack/2500);
+}
+
+void hear_command() {
+  //Serial.println("listening...");
+  record_audio();
+  //Serial.println("sending...");
+  //Serial.print("\nStarting connection to server...");
+  delay(300);
+  conn = false;
+  for (int i = 0; i < 10; i++) {
+    int val = (int)client2.connect("speech.google.com", 443, 4000);
+    Serial.print(i); Serial.print(": "); Serial.println(val);
+    if (val != 0) {
+      conn = true;
+      break;
+    }
+    Serial.print(".");
+    delay(300);
+  }
+  if (!conn) {
+    Serial.println("Connection failed!");
+    return;
+  }
+  else {
+    Serial.println("Connected to server!");
+    Serial.println(client2.connected());
+    int len = strlen(speech_data);
+    // Make a HTTP request_buffer:
+    client2.print("POST /v1/speech:recognize?key="); client2.print(API_KEY); client2.print(" HTTP/1.1\r\n");
+    client2.print("Host: speech.googleapis.com\r\n");
+    client2.print("Content-Type: application/json\r\n");
+    client2.print("cache-control: no-cache\r\n");
+    client2.print("Content-Length: "); client2.print(len);
+    client2.print("\r\n\r\n");
+    int ind = 0;
+    int jump_size = 1000;
+    char temp_holder[jump_size + 10] = {0};
+    Serial.println("sending data");
+    while (ind < len) {
+      delay(80);//experiment with this number!
+      //if (ind + jump_size < len) client.print(speech_data.substring(ind, ind + jump_size));
+      strncat(temp_holder, speech_data + ind, jump_size);
+      client2.print(temp_holder);
+      ind += jump_size;
+      memset(temp_holder, 0, sizeof(temp_holder));
+    }
+    client2.print("\r\n");
+    //Serial.print("\r\n\r\n");
+    //Serial.println("Through send...");
+    unsigned long count = millis();
+    while (client2.connected()) {
+      //Serial.println("IN!");
+      String line = client2.readStringUntil('\n');
+      Serial.print(line);
+      if (line == "\r") { //got header of response
+        //Serial.println("headers received");
+        break;
+      }
+      if (millis() - count > RESPONSE_TIMEOUT) break;
+    }
+    //Serial.println("");
+    //Serial.println("Response...");
+    count = millis();
+    while (!client2.available()) {
+      delay(100);
+      //Serial.print(".");
+      if (millis() - count > RESPONSE_TIMEOUT) break;
+    }
+    Serial.println();
+    //Serial.println("-----------");
+    memset(response_buffer, 0, sizeof(response_buffer));
+    while (client2.available()) {
+      char_append(response_buffer, client2.read(), OUT_BUFFER_SIZE);
+    }
+    //Serial.println(response); //comment this out if needed for debugging
+    char* trans_id = strstr(response_buffer, "transcript");
+    char transcript[100] = {0};
+    if (trans_id != NULL) {
+      char* foll_coll = strstr(trans_id, ":");
+      char* starto = foll_coll + 3; //starting index
+      char* endo = strstr(starto + 1, "\""); //ending index
+      int transcript_len = endo - starto;
+      
+      strncat(transcript, starto, transcript_len);
+      Serial.printf("We heard you say: %s\n",transcript);
+
+      memset(heard,0,200);
+      strcpy(heard,transcript);
+    }
+    //Serial.println("-----------");
+    client2.stop();
+    Serial.println("done");
+  }
+}
+
+void record_audio() {
+  int sample_num = 0;    // counter for samples
+  int enc_index = strlen(AUDIO_PREFIX) - 1;  // index counter for encoded samples
+  float time_between_samples = 1000000 / SAMPLE_FREQ;
+  int value = 0;
+  char raw_samples[3];   // 8-bit raw sample data array
+  memset(speech_data, 0, sizeof(speech_data));
+  sprintf(speech_data, "%s", AUDIO_PREFIX);
+  char holder[5] = {0};
+  Serial.println("starting");
+  uint32_t text_index = enc_index;
+  uint32_t start = millis();
+  time_since_sample = micros();
+  while (sample_num < NUM_SAMPLES) { //read in NUM_SAMPLES worth of audio data
+    value = analogRead(AUDIO_IN);  //make measurement
+    raw_samples[sample_num % 3] = mulaw_encode(value - 1800); //remove 1.5ishV offset (from 12 bit reading)
+    sample_num++;
+    if (sample_num % 3 == 0) {
+      base64_encode(holder, raw_samples, 3);
+      strncat(speech_data + text_index, holder, 4);
+      text_index += 4;
+    }
+    // wait till next time to read
+    while (micros() - time_since_sample <= time_between_samples); //wait...
+    time_since_sample = micros();
+  }  
+  Serial.println(millis() - start);
+  sprintf(speech_data + strlen(speech_data), "%s", AUDIO_SUFFIX);
+  Serial.println("out");
 }
